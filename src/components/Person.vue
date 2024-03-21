36068764fe18a9a161676ac297967d16ccdbdfc8
Yayufan <kamikazey2200@gmail.com>
2024-03-21 10:23:54 +0800

    Update Person.vue

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,27 +1,26 @@
 <template>
-    <div class="person">
+    <div class="person2">
         <h2>當前求和為 :{{ sum }}</h2>
         <img v-for="(dog, index) in dogList" :src="dog" :key="index" alt="">
         <button @click="addDog">再來一隻狗</button>
         <button @click="increment">增加數字</button>
     </div>
 </template>
-  
+
+
 <script lang="ts" setup name="Person235">
 
 import { type Person, type Persons } from "@/types/Person"
 import useSum from "@/hooks/person/useSum"
 import useDog from "@/hooks/person/useDog"
 
-const {sum,increment,decrement} = useSum()
-const {dogList,addDog} = useDog()
-
-
-
-
+const { sum, increment, decrement } = useSum()
+const { dogList, addDog } = useDog()
 
 </script>
-  
+
+
+
 <style scoped>
 img {
     width: 150px;
